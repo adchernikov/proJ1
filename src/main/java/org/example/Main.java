5938dc281f35f16373548556a63cff91d5e9b854
Aleksandr Chernikov <ACHernikov@inno.tech>
2022-11-23 21:59:21 +0300

    ВАЖНО!!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hell88 world!!!!");
         System.out.println("Даша-простокваша");
+        System.out.println("Даша-простокваша");
     }
 }
