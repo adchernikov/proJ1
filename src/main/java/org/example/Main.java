9375b3213356a09968281a164ae3dd6741303348
Aleksandr Chernikov <ACHernikov@inno.tech>
2022-11-23 21:44:49 +0300

    Выполлены улучшения

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hellдo world!!!!");
+        System.out.println("Hell88 world!!!!");
     }
 }
